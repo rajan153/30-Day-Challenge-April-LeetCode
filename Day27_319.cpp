78ff0f95ec5c070edbe22e170d8c7fea12dc71dd
Rajan <111041701+rajan153@users.noreply.github.com>
2023-04-27 11:26:43 +0530

    Create Day27_319.cpp

---
+++ Day27_319.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+//T.C. O(sqrtN)
+/*
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        long  ans = sqrt(n);
+        float x = ans;
+        return x;
+    }
+};
+*/
